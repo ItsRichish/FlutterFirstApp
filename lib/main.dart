52d58a3881801d0e0e1c22c6d52d5f1c71343c0c
Richish Mandal <richishm19@gmail.com>
2022-08-24 13:52:31 +0530

    Day2 compeleted

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,17 @@
 import 'package:flutter/material.dart';
+import 'home_page.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
+    int days = 30;
+    String name = "Richi";
     return MaterialApp(
-      home: Material(
-        child: Center(
-            child: Container(
-          child: Text("Welcome to my flutter app"),
-        )),
-      ),
+      home: HomePage(),
     );
   }
 }
